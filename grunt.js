939f07ffcda580dfacaa3e3b31632a2504a3c0c8
Bryan Grohman <bgrohman@gmail.com>
2012-08-31 17:18:50 -0500

    basic grunt build

---
+++ grunt.js
@@ -0,0 +1,58 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! lazy-worker - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* https://github.com/bgrohman/lazy-worker\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'Bryan Grohman; Licensed MIT */'
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/lazy-worker.js>'],
+        dest: 'dist/lazy-worker-0.1.0.min.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/lazy-worker-0.1.0.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {}
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};
